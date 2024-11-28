6fc75743fb13c0f38c1ab7de34e6a0ca00d1e6f4
Mayur Pujari <mayur.pujari@cloudstrats.com>
2024-11-28 17:46:20 +0530

    setting modified

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
