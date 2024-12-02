2aedad08c214471e791e97356bcf6e06eb55030c
Mayur Pujari <mayur.pujari@cloudstrats.com>
2024-12-03 01:11:38 +0530

    mofidy app.js

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 // const API_URL = 'http://127.0.0.1:8000/api/todos/';
-const API_URL = 'https://todo-app-production-91ad.up.railway.app/api';
+const API_URL = 'https://todo-app-production-91ad.up.railway.app/api/todos';
 
 
 
