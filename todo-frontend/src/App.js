0f2230f21193db4e665bae49942f8bf3950bedb4
Mayur Pujari <mayur.pujari@cloudstrats.com>
2024-12-03 00:48:14 +0530

    Modified api url

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/todos/';
+// const API_URL = 'http://127.0.0.1:8000/api/todos/';
+const API_URL = 'https://todo-app-production-91ad.up.railway.app/api';
+
+
 
 function App() {
     const [todos, setTodos] = useState([]);
